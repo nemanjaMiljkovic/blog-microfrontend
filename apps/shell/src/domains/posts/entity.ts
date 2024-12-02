f6c07d0da185104ee88a1ee295079ff13d8343a6
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2024-12-02 11:56:29 +0100

    add post domain entity

---
+++ apps/shell/src/domains/posts/entity.ts
@@ -0,0 +1,11 @@
+export interface Post {
+    postId: string;
+    title: string;
+    content: string;
+    authorId: string;
+    createdAt: string;
+    updatedAt: string;
+    tags: string[];
+    status: string;
+  }
+  
