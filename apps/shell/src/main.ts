3e491cedbb6cc4e536713183f24369e4f8b80f21
Nemanja Miljkovic <nmiljkovic89@gmail.com>
2024-12-02 11:53:12 +0100

    init mocks

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,1 +1,11 @@
-import('./bootstrap').catch((err) => console.error(err));
+async function initMocks() {
+  // Always initialize mocks in development
+  const { worker } = await import('./mocks/browser');
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+initMocks().then(() => {
+  import('./bootstrap').catch((err) => console.error(err));
+});
